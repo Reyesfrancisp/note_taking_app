a6f439ea773a21f67f39ad7f80c3ada382802ed0
Francis Reyes <reyesfrancisp@gmail.com>
2023-07-07 18:50:08 -0400

    cleaned up code and added more comments

--- server.js
+++ server.js
@@ -2,109 +2,111 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
-const { v4: uuid } = require("uuid")
+const { v4: uuid } = require("uuid");
 const PORT = process.env.PORT || 3333;
-//console.log(__dirname);
 
+// Serve static files from the "public" directory
 app.use(express.static("public"));
+
+// Parse JSON requests
 app.use(express.json());
 
-app.get("/api/notes", (clientRequestObject, serverResponseObject) => {
-  //read the db.json and return all saved notes as json
-  serverResponseObject.sendFile(path.join(__dirname, "db/db.json"));
-  //output the object on the page to print out the notes
-});
-
-app.get("/notes", (clientRequestObject, serverResponseObject) => {
-  //read the db.json and return all saved notes as json
-  serverResponseObject.sendFile(path.join(__dirname, "./public/notes.html"));
-  //output the object on the page to print out the notes
-});
-
-app.delete("/api/notes/:id", (clientRequestObject, serverResponseObject) => {
-  const noteId = clientRequestObject.params.id;
+// Read all saved notes from "db.json"
+app.get("/api/notes", (req, res) => {
   fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return serverResponseObject.status(500).send('Server Error');
+      return res.status(500).send('Server Error');
     }
 
+    // Parse notes data and send as JSON response
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+// Return the "notes.html" file
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// Delete a specific note by ID
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server Error');
+    }
+
+    // Parse notes data
     let notes = JSON.parse(data);
+
+    // Find the index of the note with the specified ID
     const noteIndex = notes.findIndex(note => note.id === noteId);
 
     if (noteIndex === -1) {
       // If the note is not found, return an error response
-      return serverResponseObject.status(404).send('Note not found');
+      return res.status(404).send('Note not found');
     }
 
     // Remove the note from the array
     notes.splice(noteIndex, 1);
 
+    // Write the updated notes back to "db.json"
     fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes), err => {
       if (err) {
         console.error(err);
-        return serverResponseObject.status(500).send('Server Error');
+        return res.status(500).send('Server Error');
       }
 
       // Send a success response
-      serverResponseObject.sendStatus(204);
+      res.sendStatus(204);
     });
   });
 });
 
-
-
-
-app.post("/api/notes", (clientRequestObject, serverResponseObject) => {
-  // Read the existing notes from db.json
+// Create a new note
+app.post("/api/notes", (req, res) => {
+  // Read the existing notes from "db.json"
   fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return serverResponseObject.status(500).send('Server Error');
+      return res.status(500).send('Server Error');
     }
 
+    // Parse notes data
     let notes = JSON.parse(data);
 
-    // Generate a unique ID
+    // Generate a unique ID for the new note
     const newNote = {
       id: uuid(),
-      ...clientRequestObject.body // Request body contains the new note data
+      ...req.body // Request body contains the new note data
     };
 
     // Add the new note to the array of notes
     notes.push(newNote);
 
-    // Write the updated notes back to db.json
+    // Write the updated notes back to "db.json"
     fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes), err => {
       if (err) {
         console.error(err);
-        return serverResponseObject.status(500).send('Server Error');
+        return res.status(500).send('Server Error');
       }
 
       // Send the new note as the response
-      serverResponseObject.json(newNote);
+      res.json(newNote);
     });
   });
 });
 
-// Will send the index.html file as response
-app.get("*", (clientRequestObject, serverResponseObject) => {
-  serverResponseObject.sendFile(path.join(__dirname, "./public/index.html"));
+// Return the "index.html" file for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.listen(PORT, () => console.log('Server started on port 3333.'));
-
-// Getting Started
-// The application should have a db.json file on the back end that will be used to store and retrieve notes using the fs module.
-
-// The following HTML routes should be created:
-
-// GET /notes should return the notes.html file.
-
-// GET * should return the index.html file.
-
-// The following API routes should be created:
-
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
-
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
